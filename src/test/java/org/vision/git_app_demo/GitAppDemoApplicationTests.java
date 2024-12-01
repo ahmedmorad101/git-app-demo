ebe0a6645e7464c9695b1fa8c58671e9d6146559
mourad <ahmed.morad@sssd.shj.ae>
2024-12-01 17:11:25 +0400

    update

---
+++ src/test/java/org/vision/git_app_demo/GitAppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vision.git_app_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitAppDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

2f34e3c02816476eb0e5f8cb801323c69cb5bcd4
mourad <ahmed.morad@sssd.shj.ae>
2024-12-01 20:34:58 +0400

    update HomeController

--- src/main/java/org/vision/git_app_demo/HomeController.java
+++ src/main/java/org/vision/git_app_demo/HomeController.java
@@ -5,11 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
 public class HomeController {
 
     @GetMapping
-    @RequestMapping("/s")
     public String index(){
         return "Hello World";
     }
